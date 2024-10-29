065f39984c47eb70caf3c3b343737b84bb3bb1a1
faishalshidqi <rshidqi6@gmail.com>
2024-10-29 13:51:51 +0700

    post new recipe endpoint

--- main.go
+++ main.go
@@ -1,29 +1,12 @@
 package main
 
 import (
-	"time"
-	"github.com/google/uuid"
 	"github.com/faishalshidqi/gin-introductory-proj/src/handlers"
 	"github.com/gin-gonic/gin"
 )
 
-type Recipe struct {
-	ID           uuid.UUID `json:"id"`
-	Name         string    `json:"name"`
-	Tags         []string  `json:"tags"`
-	Ingredients  []string  `json:"ingredients"`
-	Instructions []string  `json:"instructions"`
-	PublishedAt  time.Time `json:"published_at"`
-	UpdatedAt    time.Time `json:"updated_at"`
-}
-
 func main() {
 	router := gin.Default()
-	router.GET(
-		"/:name", handlers.IndexHandler,
-	)
-	router.GET(
-		"/person", handlers.PersonHandler,
-	)
+	router.POST("/recipes", handlers.PostRecipeHandler)
 	router.Run(":9000")
 }
